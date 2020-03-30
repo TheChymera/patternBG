309435f68420ece31fb2c273d622e80b23dd2403
Horea Christian <chr@chymera.eu>
2020-03-30 06:53:01 -0400

    Updated email address

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version="",
     description = "Python script that creates tile-based visual art. ",
     author = "Horea Christian",
-    author_email = "h.chr@mail.ru",
+    author_email = "chr@chymera.eu",
     url = "https://github.com/TheChymera/patternBG",
     keywords = ["artwork", "image processing"],
     py_modules = ["patternBG", "patternBG_cli"],
